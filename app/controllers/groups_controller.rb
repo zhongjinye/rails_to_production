e802369dc9a109d8b29226b39235bec95b50aa81
zhongjinye <610505041@qq.com>
2021-03-03 16:14:19 +0800

    show group's creator info

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -18,8 +18,8 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    @group.user = current_user。
-    
+    @group.user = current_user
+
     if @group.save
         redirect_to groups_path
     else
