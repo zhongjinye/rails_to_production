4a35a77f9e054fb11d19d903b46f900e6e5ca908
zhongjinye <610505041@qq.com>
2021-03-04 16:25:50 +0800

    move sqlite3 to dev group & add pg to production group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -29,57 +29,57 @@ class GroupsController < ApplicationController
     end
   end
 
-    def update
-      if @group.update(group_params)
-        redirect_to groups_path, notice: "Update Success"
-      else
-        render :edit
-      end
+  def update
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
   end
 
-    def destroy
-       @group.destroy
-       redirect_to groups_path, alert: "Group deleted"
+  def destroy
+     @group.destroy
+     redirect_to groups_path, alert: "Group deleted"
+  end
+
+  def join
+   @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
     end
 
-    def join
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    @group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+ private
+
+ def find_group_and_check_permission
      @group = Group.find(params[:id])
 
-      if !current_user.is_member_of?(@group)
-        current_user.join!(@group)
-        flash[:notice] = "加入本讨论版成功！"
-      else
-        flash[:warning] = "你已经是本讨论版成员了！"
-      end
-
-      redirect_to group_path(@group)
-    end
-
-    def quit
-      @group = Group.find(params[:id])
-
-      if current_user.is_member_of?(@group)
-        current_user.quit!(@group)
-        flash[:alert] = "已退出本讨论版！"
-      else
-        flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
-      end
-
-      redirect_to group_path(@group)
-    end
-
-   private
-
-   def find_group_and_check_permission
-       @group = Group.find(params[:id])
-
-       if current_user != @group.user
-             redirect_to root_path, alert: "You have no permission."
-           end
-         end
-
-   def group_params
-     params.require(:group).permit(:title, :description)
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+   end
  end
+
+  def group_params
+     params.require(:group).permit(:title, :description)
+  end
 
 end
