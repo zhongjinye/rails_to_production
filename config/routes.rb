70a5e81c40aa3b730b82bada3ee8fb9784704598
zhongjinye <610505041@qq.com>
2021-03-04 14:51:10 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   end
   root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :account do
+    resources :groups
+  end
 end
