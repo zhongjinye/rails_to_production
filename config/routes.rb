9d87d062a0a46c816861ee1675b1d038ba7a85a8
zhongjinye <610505041@qq.com>
2021-03-04 14:59:50 +0800

    implement my posts

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :account do
     resources :groups
+    resources :posts
   end
 end
